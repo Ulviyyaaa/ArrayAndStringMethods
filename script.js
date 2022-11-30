dd510e5f80b6094dbd00172d688662ee847bed4a
Ulviyyaaa <tu7s25j8q@code.edu.az>
2022-12-01 00:16:43 +0400

    add task

---
+++ script.js
@@ -0,0 +1,36 @@
+//______________________________________________________________________
+
+// task1
+
+// const average = arr => arr.reduce((a, b) => a + b, 0) / arr.length;
+// const result = average([1, 25, 244, 44, 55, 22, 64]);
+// console.log(result);
+
+//_______________________________________________________________________
+
+// task2
+
+// let arr=[1,5,2,7] 
+// arr.push(11)
+// console.log(arr);
+
+//_______________________________________________________________________
+
+//task3
+
+// let string = "Salam Necesen";
+// let sum = 0;
+// function count() {
+//     for (let i = 0; i < string.length; i++) {
+//         if (string[i] == "N") {
+//             sum++
+//         }
+//         else if (string[i] == "n") {
+//             sum++
+//         }
+//     }
+//     return sum
+// }
+// console.log(count(string));
+
+//_______________________________________________________________________
